4d1dfa6243111c636588ca1f712b6b586b605d15
MarkV <mark.vazquez123@gmail.com>
2022-11-30 18:25:32 -0600

    Removed random method/clutter.

--- 3321_Prototype/Source.cpp
+++ 3321_Prototype/Source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Menu.h"
-void test123();
+
 int main()
 {
 	Menu menu;
@@ -8,8 +8,4 @@ int main()
 	menu.display_main_menu();
 
 	return 0;
-}
-void test123()
-{
-	std::cout << 123 << std::endl;
 }
