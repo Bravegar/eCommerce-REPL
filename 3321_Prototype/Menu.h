028e8eaa06c88cc1789ac65c0ff2c75f38e92171
jarryhai <118564537+jarryhai@users.noreply.github.com>
2022-12-07 22:06:01 -0600

    Added userList and Display all users

--- 3321_Prototype/Menu.h
+++ 3321_Prototype/Menu.h
@@ -13,21 +13,17 @@ class Menu
 public:
 
 	Menu();
-	void display_login_menu();
 	void display_main_menu();
 	void display_menu_option(int);
 	void display_all_cards();
+	void display_all_users();
 	void single_card_display(int, std::vector<Card>);
 	void init_cards();
+	void init_users();
 	void addCard();
 	std::vector<Card> FilterCardListByPrice();
 	void displayCardList(std::vector<Card>);
 	std::vector<Card> FilterCardListByName();
 	std::vector<Card> FilterCardListByType();
-	void create_new_user();
-	bool user_login(std::string, std::string);
-	void init_user_list();
-	bool check_user_exists(std::string);
-
 };
 
