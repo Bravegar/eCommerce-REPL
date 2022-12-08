a6e435b79d533885cce227676daf8142b33b28f5
ajcarter19 <63877382+ajcarter19@users.noreply.github.com>
2022-12-07 19:46:06 -0600

    Update Admin.h

--- 3321_Prototype/Admin.h
+++ 3321_Prototype/Admin.h
@@ -1,13 +1,20 @@
+#pragma once
 #include <string>
 #include <vector>
 using namespace std;
 
 class Admin
 {
-        int adminID, phone, selection;
-        string FName, LName, address, email;
-        bool banStatus;
-        
+    int adminID, phone, selection;
+    string FName, LName, address, email;
+    bool banStatus;
+
 public:
-        void AdminManageUser(int);
+    void AdminManageUser(int);
+    string getFName (string FName);
+    string getLName (string LName);
+    string getAddress (string address);
+    string getPhone (int phone);
+    string getEmail (string email);
+    bool setBan();
 };
