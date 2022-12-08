e07fec256532a2388996fa235ff25a235a362ce7
ajcarter19 <63877382+ajcarter19@users.noreply.github.com>
2022-12-07 18:40:51 -0600

    Update Admin.cpp

--- 3321_Prototype/Admin.cpp
+++ 3321_Prototype/Admin.cpp
@@ -1,7 +1,7 @@
 #include "Admin.h"
 #include <iostream>
 
-void Admin::ManageUser(int select)
+void Admin::AdminManageUser(int select)
 {
   cout << "Enter (1) to update First name,\nEnter (2) to update Last name,\nEnter (3) to update Address info,\n"
     "Enter (4) to update Phone number,\nEnter (5) to update Email,\nEnter (6) to update ban status."
@@ -10,5 +10,24 @@ void Admin::ManageUser(int select)
   switch(select)
   {
     case 1:
+      setFName(input);
+      break;
+    case 2:
+      setLName(input);
+      break:
+    case 3:
+      setAddress(input);
+      break;
+    case 4:
+      setPhone(input);
+      break;
+    case 5:
+      setEmail(input);
+      break;
+    case 6:
+      setBan(input);
+      break;
+    default:
+      cout << "Please enter a valid selection." << endl;
   }
     
