1a6840f5e8b3182de85ee1f730f9ec4cd5f926e6
ajcarter19 <63877382+ajcarter19@users.noreply.github.com>
2022-12-07 18:31:20 -0600

    Update Admin.cpp

--- 3321_Prototype/Admin.cpp
+++ 3321_Prototype/Admin.cpp
@@ -1,1 +1,14 @@
 #include "Admin.h"
+#include <iostream>
+
+void Admin::ManageUser(int select)
+{
+  cout << "Enter (1) to update First name,\nEnter (2) to update Last name,\nEnter (3) to update Address info,\n"
+    "Enter (4) to update Phone number,\nEnter (5) to update Email,\nEnter (6) to update ban status."
+  cin >> select;
+  
+  switch(select)
+  {
+    case 1:
+  }
+    
