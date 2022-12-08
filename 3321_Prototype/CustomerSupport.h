6fd2b26637fdf3cbe805576a81a41b08c040966e
IssaDude02 <119621980+IssaDude02@users.noreply.github.com>
2022-12-07 19:05:32 -0600

    Update CustomerSupport.h

--- 3321_Prototype/CustomerSupport.h
+++ 3321_Prototype/CustomerSupport.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <string>
-
+#include <iostream>
 using namespace std;
 
-string getInquiry();
-void setInquiry(string user_input);
-bool getIssueResolved();
-void updateIssueStatus(int user_input);
+class CustomerSupport {
+private:
+	bool issueResolved = false;
+	string inquiry = "";
+public:
+	string getInquiry();
+	void setInquiry(string user_input);
+	bool getIssueResolved();
+	void updateIssueStatus(int user_input);
+};
+
