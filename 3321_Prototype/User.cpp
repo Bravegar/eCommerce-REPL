e96dd9c221e1031713c5efc868b9b3cb07cdc524
Pop <popdrop44@gmail.com>
2022-12-07 20:46:16 -0600

    changes user class

--- 3321_Prototype/User.cpp
+++ 3321_Prototype/User.cpp
@@ -1,68 +1,130 @@
 #include "User.h"
-#include<string>
-#include<iostream>
 
-using namespace std;
+User::User(std::string fN, std::string lN, std::string e, std::string a, std::string u, std::string p, int ph)
+{
+	fName = fN;
+	lName = lN;
+	email = e;
+	address = a;
+	username = u;
+	password = p;
+	phone = ph;
+	sellerRating = 0;
+	buyerRating = 0;
+	banStatus = false;
+}
 
-	string User::getFName() 
-	{
-		return fName;
-	}
-	void User::setFName(string user_input) {
-		fName = user_input;
-	}
-	string User::getLName() {
-		return lName;
-	}
-	void User::setLName(string user_input) {
-		lName = user_input;
-	}
-	string User::getAddress() {
-		return address;
-	}
-	void User::setAddress(string user_input) {
-		address = user_input;
-	}
-	int User::getPhone() {
-		return phone;
-	}
-	void User::setPhone(int user_input) {
-		phone = user_input;
-	}
-	string User::getEmail() {
-		return email;
-	}
-	void User::setEmail(string user_input) {
-		email = user_input;
-	}
-	double User::getSellerRating() {
-		return sellerRating;
-	}
-	void User::setSellerRating(double user_input) {
-		sellerRating = user_input;
-	}
-	double User::getBuyerRating() {
-		return buyerRating;
-	}
-	void User::setBuyerRating(double user_input) {
-		buyerRating = user_input;
-	}
-	string User::getUsername() {
-		return username;
-	}
-	void User::setUsername(string user_input) {
-		username = user_input;
-	}
-	string User::getPassword() {
-		return password;
-	}
-	void User::setPassword(string user_input) {
-		password = user_input;
-	}
-	bool User::getBanStatus() {
-		return banStatus;
-	}
-	void User::setBanStatus(bool user_input) {
-		banStatus = user_input;
-	}
+//============ accessor functions
+std::string User::getFName()
+{
+	return fName;
+}
 
+std::string User::getLName()
+{
+	return lName;
+}
+
+std::string User::getEmail()
+{
+	return email;
+}
+
+std::string User::getAddress()
+{
+	return address;
+}
+
+std::string User::getUsername()
+{
+	return username;
+}
+
+std::string User::getPassword()
+{
+	return password;
+}
+
+int User::getPhone()
+{
+	return phone;
+}
+
+bool User::getBanStatus()
+{
+	return banStatus;
+}
+
+double User::getSellerRating()
+{
+	return sellerRating;
+}
+
+double User::getBuyerRating()
+{
+	return buyerRating;
+}
+
+//============ mutator functions
+void User::setFName(std::string user_input)
+{
+	fName = user_input;
+}
+
+void User::setLName(std::string user_input)
+{
+	lName = user_input;
+}
+
+void User::setAddress(std::string user_input)
+{
+	address = user_input;
+}
+
+void User::setPhone(int user_input)
+{
+	phone = user_input;
+}
+
+void User::setEmail(std::string user_input)
+{
+	email = user_input;
+}
+
+void User::setSellerRating(double user_input)
+{
+	sellerRating = user_input;
+}
+
+void User::setBuyerRating(double user_input)
+{
+	buyerRating = user_input;
+}
+
+void User::setUsername(std::string user_input)
+{
+	username = user_input;
+}
+
+void User::setPassword(std::string user_input)
+{
+	password = user_input;
+}
+
+void User::setBanStatus(bool user_input)
+{
+	banStatus = user_input;
+}
+
+void User::display()
+{
+	std::cout << "User Information\n";
+	std::cout << "First Name: " << getFName();
+	std::cout << "Last Name: " << getLName();
+	std::cout << "Username: " << getUsername();
+	std::cout << "Email: " << getEmail();
+	std::cout << "Address: " << getAddress();
+	std::cout << "Phone Number: " << getPhone();
+	std::cout << "Seller Rating: " << getSellerRating();
+	std::cout << "Buyer Rating: " << getBuyerRating();
+}
--- 3321_Prototype/User.h
+++ 3321_Prototype/User.h
@@ -1,45 +1,56 @@
 #pragma once
+#include<iostream>
 #include<string>
 #include<vector>
 
-using namespace std;
+
 class User 
 {
 private:
-	string fName, lName, address,
+	std::string fName, lName, address,
 		email, username, password;
 	int phone;
 	double sellerRating, buyerRating;
 	bool banStatus;
-
 public:
-	User() 
+	//============ constructors
+	User()
 	{
-		fName = "John"; lName = "Doe";
-		address = "1234 willow peek";
-		username = "TempUsername"; password = "TempPass";
-		phone = -1; sellerRating = -1;
-		buyerRating = -1; banStatus = false;
+		fName = "";
+		lName = "";
+		email = "";
+		address = "";
+		username = "";
+		password = "";
+		phone = -1;
+		sellerRating = -1;
+		buyerRating = -1;
+		banStatus = false;
 	}
-	string getFName();
-	void setFName(string user_input);
-	string getLName();
-	void setLName(string user_input);
-	string getAddress();
-	void setAddress(string user_input);
+	User(std::string, std::string, std::string, std::string, std::string, std::string, int);
+	//============ accessor functions
+	std::string getFName();
+	std::string getLName();
+	std::string getEmail();
+	std::string getAddress();
+	std::string getUsername();
+	std::string getPassword();
 	int getPhone();
-	void setPhone(int user_input);
-	string getEmail();
-	void setEmail(string user_input);
 	double getSellerRating();
-	void setSellerRating(double user_input);
 	double getBuyerRating();
-	void setBuyerRating(double user_input);
-	string getUsername();
-	void setUsername(string user_input);
-	string getPassword();
-	void setPassword(string user_input);
 	bool getBanStatus();
+	//============ mutator functions
+	void setFName(std::string user_input);
+	void setLName(std::string user_input);
+	void setAddress(std::string user_input);
+	void setPhone(int user_input);
+	void setEmail(std::string user_input);
+	void setSellerRating(double user_input);
+	void setBuyerRating(double user_input);
+	void setUsername(std::string user_input);
+	void setPassword(std::string user_input);
 	void setBanStatus(bool user_input);
+	//=========== display
+	void display();
 };
 
