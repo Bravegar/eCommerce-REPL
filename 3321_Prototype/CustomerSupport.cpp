88c92c70ca7eeb8d1e3edc7003978b55ce9169bf
IssaDude02 <119621980+IssaDude02@users.noreply.github.com>
2022-12-07 19:05:12 -0600

    Update CustomerSupport.cpp

--- 3321_Prototype/CustomerSupport.cpp
+++ 3321_Prototype/CustomerSupport.cpp
@@ -1,21 +1,21 @@
 #include "CustomerSupport.h"
 #include<string>
+#include<iostream>
+using namespace std;
 
-bool issueResolved = false;
-string inquiry;
-
-string getInquiry()
+string CustomerSupport::getInquiry()
 {
 	return inquiry;
 }
-void setInquiry(string user_input)
+
+void CustomerSupport::setInquiry(string user_input)
 {
 	/*The inquiry is a string so that the user can type in
 	any problem they have*/
 	inquiry = user_input;
 }
 
-void updateIssueStatus(int user_input)
+void CustomerSupport::updateIssueStatus(int user_input)
 {
 	/*Assuming that the user_input can be 1 for true 
 	and 0 for false, it's easier to have the issue 
@@ -25,7 +25,7 @@ void updateIssueStatus(int user_input)
 	else
 		issueResolved = false;
 }
-bool getIssueResolved()
+bool CustomerSupport::getIssueResolved()
 {
 	return issueResolved;
 }
