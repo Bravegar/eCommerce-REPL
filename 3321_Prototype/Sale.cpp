ec8643a8725d3e708769a15b8fb338855b0d78c9
IssaDude02 <119621980+IssaDude02@users.noreply.github.com>
2022-12-07 19:22:52 -0600

    Update Sale.cpp

---
+++ 3321_Prototype/Sale.cpp
@@ -0,0 +1,45 @@
+#include "Sales.h"
+#include <iostream>
+
+using namespace std;
+
+int Sales::getSaleNumber()
+{
+	return saleNumber;
+}
+
+void Sales::setSaleNumber(int user_input)
+{
+	saleNumber = user_input;
+}
+
+int Sales::getCustomerID()
+{
+	return customerID;
+}
+
+void Sales::setCustomerID(int user_input)
+{
+	customerID = user_input;
+}
+
+int Sales::getSellerID()
+{
+	return sellerID;
+}
+
+void Sales::setSellerID(int user_input) {
+	sellerID = user_input;
+}
+
+void Sales::printAll()
+{
+	/*Display << “Sale Information : ” << “\n”;
+	Display << “Sale Number : ” << SalesClass.saleNumber << “\n”;
+	Display << “Customer ID : ” << SalesClass.customerID << “\n”;
+	Display << “Seller ID : ” << SalesClass.sellerfID << “\n”;*/
+	cout << "Sale information: \n"
+		<< "Sale number - " << getSaleNumber() << endl
+		<< "Customer ID - " << getCustomerID() << endl
+		<< "Seller ID - " << getSellerID() << endl;
+}
